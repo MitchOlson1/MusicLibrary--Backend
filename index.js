dd730f1feac5d266f084babab76c18e46675989c
Mitch Olson <mitch3344@gmail.com>
2022-03-31 20:05:05 -0500

    Created function to recieve/GET songs, also created function to recieve/GET a song

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 //Imports
 const express = require("express");
+const context = require("./repository/repository-wrapper")
 const app = express();
 
 //Middle
 app.use(express.json());
+
+
+//Endpoints
+//GET = http://localhost:5005/api/songs
+app.get("/api/songs", (req, res) => {
+   const songs = context.songs.findAllSongs();
+    return res.send(songs);
+});
+
+//GET ID = http://localhost:5005/api/songs/:id
+app.get("/api/songs/:id", (req, res) => {
+    const id = req.params.id;
+    const song = context.songs.findSongById(id);
+    return res.send(song);
+});
 
 //Starting a Server
 const PORT = process.env.PORT || 5005;
